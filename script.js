5aeb163ad903dcfc763ae07dd5e74f00e6f14d9d
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-10-10 14:38:43 +0530

    first commit for ws

---
+++ script.js
@@ -0,0 +1,4 @@
+// document.getElementById("title").innerHTML = "New Title";
+// const a = document.getElementById("myDIV");
+const button = document.querySelector(".checked");
+console.log(button);
