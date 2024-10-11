ceb32b447a3de04617eeb970dd6ee8da5802cf40
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-10-11 12:25:21 +0530

    extra note file with js code

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-// document.getElementById("title").innerHTML = "New Title";
-// const a = document.getElementById("myDIV");
-const button = document.querySelector(".checked");
-console.log(button);
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}   
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () { 
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+// checked class code for line throgh
+var list = document.querySelector("ul");
+// console.log(list);
+
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
